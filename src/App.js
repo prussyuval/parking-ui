58628dc96486dc43b0645105c3e54d4d93596947
yuvalpruss <yuval@wenrix.com>
2021-11-08 23:39:36 +0200

    Bug fix

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ function App() {
           }
           const selectedLotId = PARKING_LOT_IDS[selectedLotName];
 
-          const response = await ServerRequest.get('status', {lotId: selectedLotId});
+          const response = await ServerRequest.get('status', {lot_id: selectedLotId});
           setStatistics(response.data);
           // setStatistics({"success":true,"data":{"current":"EMPTY","stored_status":{"08/11/2021 22:43":1,"11/10/2021 22:43":2},"time":"08/11/2021 22:43"}});
         }
