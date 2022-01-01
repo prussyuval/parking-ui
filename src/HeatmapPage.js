a1ca7bbd29e12f120d82f44b51bca4f406cab75c
yuvalpruss <yuval@wenrix.com>
2022-01-01 21:50:11 +0200

    Save me

--- src/HeatmapPage.js
+++ src/HeatmapPage.js
@@ -2,23 +2,57 @@ import CircularProgress from "@mui/material/CircularProgress";
 import {useEffect, useState} from "react";
 import ServerRequest from "./ServerRequest";
 import ApexChart from "./helpers/Heatmap";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 const HeatmapPage = (props) => {
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [parkingLot, setParkingLotId] = useState(null);
     const [heatMapData, setHeatMapData] = useState(null);
 
     useEffect(() => {
-      async function refreshOnMount() {
+      async function getHeatMapData() {
 
-          const response = await ServerRequest.get('lot-heat-map', {lot_id: 45});
+          const response = await ServerRequest.get('lot-heat-map', {lot_id: parkingLot});
           console.log(response);
           setHeatMapData(response["data"]["heat_map"]);
           setLoading(false);
         }
 
-      refreshOnMount();
-  }, []);
+      getHeatMapData();
+  }, [parkingLot]);
+
+    const searchParkingLot = (value) => {
+        setLoading(true);
+        setParkingLotId(value);
+    }
+
+    if (parkingLot === null) {
+        return (
+        <div>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={parkingLot}
+                  label="Age"
+                  onChange={searchParkingLot}
+                >
+                  <MenuItem value={40}>Ravnitski</MenuItem>
+                  <MenuItem value={45}>Tel nordau</MenuItem>
+                  <MenuItem value={94}>Habima theatre</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+        </div>
+        );
+    }
 
     if (loading === true) {
         return (
